351acb957b0b4bd16770e6c40b378eb2e8b315da
elnigerian <aaodutayo@gmail.com>
2021-03-06 06:34:28 -0500

    initial commit

---
+++ automation.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+'''
+This is an attempt to create code to scrape info from a site.
+'''
+
